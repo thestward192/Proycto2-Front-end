3f9208f687bd5349e77766f607311b3ef07d4ce9
TosTar26 <ozsanchez2004@gmail.com>
2024-06-05 00:59:16 -0600

    Arreglando el 404

--- src/Components/ComponentsPages/NotFoundPage.tsx
+++ src/Components/ComponentsPages/NotFoundPage.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const NotFoundPage: React.FC = () => {
     return (
-        <div>
-            <h1>404 - Página no encontrada</h1>
-            <p>Lo siento, no tienes permiso para acceder a esta página.</p>
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+            <h1 className="text-5xl font-bold text-red-600 mb-4">404 - Página no encontrada</h1>
+            <p className="text-xl text-gray-700">Lo siento, no tienes permiso para acceder a esta página.</p>
         </div>
     );
 };

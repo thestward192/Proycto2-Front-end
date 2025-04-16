9e92bb997446897d105eaea0c9e81902d35217a2
TosTar21 <oscar.zuniga.sanchez@est.una.ac.cr>
2025-04-15 22:30:18 -0600

    changing url images

--- src/Components/ComponentsPages/LandingPage.tsx
+++ src/Components/ComponentsPages/LandingPage.tsx
@@ -86,7 +86,7 @@ const LandingPage = () => {
               <p className="text-gray-500 break-line">Ofrecemos tratamientos dentales de alta calidad para mantener tu salud bucal.</p>
             </div>
             <div className="card bg-gray-300 rounded-md shadow-md p-4 flex flex-col items-center">
-              <img src="https://plustatic.com/6103/conversions/tipos-pediatras-default.jpg" alt="Pediatría" className="w-full rounded-md mb-4" />
+              <img src="https://i.ibb.co/xqP5JgKF/pediatria.webp" alt="Pediatría" className="w-full rounded-md mb-4" />
               <h3 className="text-gray-700 font-bold">Pediatría</h3>
               <p className="text-gray-500 break-line">Nos especializamos en el cuidado de la salud de los niños, desde recién nacidos hasta adolescentes.</p>
             </div>
